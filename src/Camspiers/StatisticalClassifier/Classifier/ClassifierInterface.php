addb2274fabfef2c8eb92c8a0897bd82e59f6307
Cam Spiers <cameron@heyday.co.nz>
2013-02-19 17:33:38 +1300

    MINOR: Remove comments from classifier interface

--- src/Camspiers/StatisticalClassifier/Classifier/ClassifierInterface.php
+++ src/Camspiers/StatisticalClassifier/Classifier/ClassifierInterface.php
@@ -4,8 +4,5 @@ namespace Camspiers\StatisticalClassifier\Classifier;
 
 interface ClassifierInterface
 {
-    // public function getSource();
-    // public function prepare();
     public function classify($document);
-    // public function train($category, $document);
 }
