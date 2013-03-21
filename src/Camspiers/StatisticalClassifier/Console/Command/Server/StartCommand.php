0c525eae169358f82a7369a8df1bc479cc09506a
Cam Spiers <cameron@heyday.co.nz>
2013-03-21 13:57:39 +1300

    UPDATE: Docs and refactor into method per request type

--- src/Camspiers/StatisticalClassifier/Console/Command/Server/StartCommand.php
+++ src/Camspiers/StatisticalClassifier/Console/Command/Server/StartCommand.php
@@ -17,9 +17,9 @@ use Symfony\Component\Console\Output;
 use Camspiers\StatisticalClassifier\Console\Command\Command;
 use Camspiers\StatisticalClassifier\Index;
 
-use React\EventLoop\Factory;
-use React\Socket\Server as SocketServer;
-use React\Http\Server as HttpServer;
+use React\EventLoop;
+use React\Socket;
+use React\Http;
 
 /**
  * @author Cam Spiers <camspiers@gmail.com>
@@ -27,7 +27,15 @@ use React\Http\Server as HttpServer;
  */
 class StartCommand extends Command
 {
-    protected $containers = array();
+    /**
+     * Holds classifier instances 
+     * @var array
+     */
+    protected $classifiers = array();
+    /**
+     * Holds index instances
+     * @var array
+     */
     protected $indexes = array();
     /**
      * Configure the commands options
@@ -61,15 +69,15 @@ class StartCommand extends Command
      */
     protected function execute(Input\InputInterface $input, Output\OutputInterface $output)
     {
-
+        // Set a dummy index
         $this->getContainer()->set(
             'index.index',
             new Index\Index()
         );
 
-        $http = new HttpServer(
-            $socket = new SocketServer(
-                $loop = Factory::create()
+        $http = new Http\Server(
+            $socket = new Socket\Server(
+                $loop = EventLoop\Factory::create()
             )
         );
 
@@ -86,76 +94,23 @@ class StartCommand extends Command
         $loop->run();
 
     }
-
-    public function serve($request, $response)
-    {
-        $query = $request->getQuery();
-        $path = $request->getPath();
-
-        switch ($path) {
+    /**
+     * Serve a request
+     * @param  Http\Request $request  The request object
+     * @param  Http\Response $response The response object
+     * @return null
+     */
+    public function serve(
+        Http\Request $request,
+        Http\Response $response
+    ) {
+        switch ($request->getPath()) {
             case '/classify':
             case '/classify/':
-
-                if (isset($query['index'])) {
-
-                    $classifierType = isset($query['classifier']) ? $query['classifier'] : 'classifier.naive_bayes';
-
-                    if (isset($this->classifiers[$classifierType])) {
-                        $classifier = $this->classifiers[$classifierType];
-                    } else {
-                        $this->classifiers[$classifierType] = $classifier = $this->getContainer()->get($classifierType);
-                    }
-
-                    if (!isset($this->indexes[$query['index']]) || (isset($query['fresh']) && $query['fresh'])) {
-                        $this->indexes[$query['index']] = new Index\CachedIndex(
-                            $query['index'],
-                            $this->container->get('cache')
-                        );
-                    }
-
-                    $classifier->setIndex($this->indexes[$query['index']]);
-
-                    $response->writeHead(
-                        200,
-                        array(
-                            'Content-Type' => 'application/json'
-                        )
-                    );
-
-                    if (isset($query['document'])) {
-
-                        $response->end(
-                            json_encode(
-                                array(
-                                    'category' => $classifier->classify($query['document'])
-                                )
-                            )
-                        );
-
-                    } else {
-
-                        $request->on(
-                            'data',
-                            function ($document) use ($response, $classifier) {
-                                $response->end(
-                                    json_encode(
-                                        array(
-                                            'category' => $classifier->classify($document)
-                                        )
-                                    )
-                                );
-                            }
-                        );
-
-                    }
-
-                } else {
-
-                    $response->writeHead(400, array('Content-Type' => 'text/plain'));
-                    $response->end('Bad request an index must be specified');
-
-                }
-
+                $this->classify(
+                    $request,
+                    $response
+                );
                 break;
             default:
                 $response->writeHead(404, array('Content-Type' => 'text/plain'));
@@ -163,5 +118,79 @@ class StartCommand extends Command
                 break;
 
         }
+    }
+    /**
+     * Respond to a classify request
+     * @param  Http\Request $request  The request object
+     * @param  Http\Response $response The response object
+     * @return null
+     */
+    protected function classify(
+        Http\Request $request,
+        Http\Response $response
+    ) {
+                
+        $query = $request->getQuery();
+
+        if (isset($query['index'])) {
+
+            $classifierType = isset($query['classifier']) ? $query['classifier'] : 'classifier.naive_bayes';
+
+            if (isset($this->classifiers[$classifierType])) {
+                $classifier = $this->classifiers[$classifierType];
+            } else {
+                $this->classifiers[$classifierType] = $classifier = $this->getContainer()->get($classifierType);
+            }
+
+            if (!isset($this->indexes[$query['index']]) || (isset($query['fresh']) && $query['fresh'])) {
+                $this->indexes[$query['index']] = new Index\CachedIndex(
+                    $query['index'],
+                    $this->container->get('cache')
+                );
+            }
+
+            $classifier->setIndex($this->indexes[$query['index']]);
+
+            $response->writeHead(
+                200,
+                array(
+                    'Content-Type' => 'application/json'
+                )
+            );
+
+            if (isset($query['document'])) {
+
+                $response->end(
+                    json_encode(
+                        array(
+                            'category' => $classifier->classify($query['document'])
+                        )
+                    )
+                );
+
+            } else {
+
+                $request->on(
+                    'data',
+                    function ($document) use ($response, $classifier) {
+                        $response->end(
+                            json_encode(
+                                array(
+                                    'category' => $classifier->classify($document)
+                                )
+                            )
+                        );
+                    }
+                );
+
+            }
+
+        } else {
+
+            $response->writeHead(400, array('Content-Type' => 'text/plain'));
+            $response->end('Bad request an index must be specified');
+
+        }
+
     }
 }
