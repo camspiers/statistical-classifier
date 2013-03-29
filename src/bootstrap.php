e40205d22da931739a9822f5fa2930c372d2f6fe
Cam Spiers <camspiers@gmail.com>
2013-03-30 01:27:58 +1300

    UPDATE: Support installation as a vendor

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,23 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$files = array(
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php'
+);
+
+$loader = false;
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        $loader = require_once $file;
+        break;
+    }
+}
+
+if (!$loader instanceof \Composer\Autoload\ClassLoader) {
+    echo 'You must first install the vendors using composer.' . PHP_EOL;
+    exit(1);
+}
 
 use Camspiers\StatisticalClassifier\Config\Config;
 use Camspiers\StatisticalClassifier\Console\Command\GenerateContainerCommand;
