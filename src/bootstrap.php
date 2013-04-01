6b7086e23b9eebda9118ffb5390fbc25c2e9a4f7
Cam Spiers <camspiers@gmail.com>
2013-04-01 23:07:45 +1300

    UPDATE: Some docs, and a script to generate the container post autoload dump

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -14,6 +14,9 @@ $files = array(
     __DIR__ . '/../../../autoload.php'
 );
 
+/**
+ * Loop through the potential autoload locations
+ */
 foreach ($files as $file) {
     if (file_exists($file)) {
         $loader = require_once $file;
@@ -21,17 +24,29 @@ foreach ($files as $file) {
     }
 }
 
-Config::setRunPath(dirname(dirname(realpath($file))));
-Config::setClassifierPath(dirname(__DIR__));
-
+/**
+ * If the autoloader isn't returning a autoload then error
+ */
 if (is_null($loader) || !$loader instanceof ClassLoader) {
     echo 'You must first install the vendors using composer.' . PHP_EOL;
     exit(1);
 }
 
+/**
+ * Based on where the autoloader was found set the run path
+ */
+Config::setRunPath(dirname(dirname(realpath($file))));
+/**
+ * Set the classifier packages path
+ */
+Config::setClassifierPath(dirname(__DIR__));
+
 $containerClass = Config::getOption('container_class');
 $containerDir = Config::getOptionPath('container_dir');
 
+/**
+ * If the container doesn't exist generate one before requiring it
+ */
 if (!file_exists("$containerDir/$containerClass.php")) {
     $command = new GenerateContainerCommand();
     $command->run(
@@ -40,4 +55,7 @@ if (!file_exists("$containerDir/$containerClass.php")) {
     );
 }
 
+/**
+ * Require the container
+ */
 require_once "$containerDir/$containerClass.php";
