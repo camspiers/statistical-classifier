58987c0490a2c4d11352e163559540d0943e2a8f
Corentin Larose <corentin.larose@qapa.com>
2014-01-02 20:24:22 +0100

    Added tokenization capability for accented languages

--- src/Camspiers/StatisticalClassifier/Tokenizer/Word.php
+++ src/Camspiers/StatisticalClassifier/Tokenizer/Word.php
@@ -22,9 +22,6 @@ class Word implements TokenizerInterface
      */
     public function tokenize($document)
     {
-        $matches = array();
-        preg_match_all("/[\w]+/", $document, $matches, PREG_PATTERN_ORDER);
-
-        return isset($matches[0]) ? $matches[0] : array();
+        return preg_split('/\PL+/u', $document, null, PREG_SPLIT_NO_EMPTY);
     }
 }
